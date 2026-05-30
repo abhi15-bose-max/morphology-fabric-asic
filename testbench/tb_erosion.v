8cac1fe17fdac2e119d9e32d8cf0cb58a479134a
abhi15-bose-max <abhinavbasu156@gmail.com>
2026-05-30 22:23:34 +0530

    Update tb_erosion.v

--- testbench/tb_erosion.v
+++ testbench/tb_erosion.v
@@ -1,3 +1,10 @@
+// ============================================================================
+// Copyright (c) 2026 Abhinav Basu (abhi15-bose-max)
+// This source code is licensed under the MIT license found in the 
+// LICENSE file in the root directory of this source tree.
+// ============================================================================
+
+
 `timescale 1ns/1ps
 
 module tb_erosion;
