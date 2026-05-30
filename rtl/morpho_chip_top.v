3f8549d1d84c5c048cf36ed1f4903335646af83e
abhi15-bose-max <abhinavbasu156@gmail.com>
2026-05-30 22:22:06 +0530

    Update morpho_chip_top.v

--- rtl/morpho_chip_top.v
+++ rtl/morpho_chip_top.v
@@ -1,3 +1,10 @@
+// ============================================================================
+// Copyright (c) 2026 Abhinav Basu (abhi15-bose-max)
+// This source code is licensed under the MIT license found in the 
+// LICENSE file in the root directory of this source tree.
+// ============================================================================
+
+
 module morpho_chip_top (
 
     input clk,
